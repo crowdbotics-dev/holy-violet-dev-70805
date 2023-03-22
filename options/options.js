284610d4541e31b81c0f9cf2709f086d5b2a07ba
Quan Nguyen <quan.nguyen+test@crowdbotics.com>
2023-03-22 16:10:30 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "holy-violet-dev-70805",
-  url: "https://holy-violet-dev-70805.botics.co",
-  api: "https://holy-violet-dev-70805.botics.co/api/v1"
-}
+  "name": "holy-violet-dev-70805",
+  "url": "https://holy-violet-dev-70805.botics.co",
+  "api": "https://holy-violet-dev-70805.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
